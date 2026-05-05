c969350abbda7e07e7b29461611ccc8b0b941a23
Mahipal Panwar <mahipalpanwar6354@gmail.com>
2026-05-05 14:31:10 +0530

    connection for db

---
+++ controller/controller.go
@@ -0,0 +1,6 @@
+package controller
+
+const connectionString = "mongodb+srv://mahipal:mongodb@123123@cluster0.qskv2lv.mongodb.net/?appName=Cluster0"
+const dbname = "netflix"
+const colname = "watchlist"
+
