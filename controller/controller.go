4abde8523ddf50c570170dd511a9ac9cea0882a0
Mahipal Panwar <mahipalpanwar6354@gmail.com>
2026-05-07 14:19:54 +0530

    update one record

--- controller/controller.go
+++ controller/controller.go
@@ -6,6 +6,8 @@ import (
 	"log"
 
 	"github.com/mahipalpanwar-7/RestAPI-MongoDB/model"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
@@ -50,3 +52,19 @@ func insertOneMovie(movie model.Netflix) {
 
 	fmt.Println("one movie inserted in database with id:", inserted.InsertedID)
 }
+
+// update one movie
+
+func updateOneMovie(movieID string){
+	id,_ := primitive.ObjectIDFromHex(movieID)
+	filter := bson.M{"_id":id}
+	update := bson.M{"$set": bson.M{"watched":true}}
+
+	result, err := collection.UpdateOne(context.Background(),filter,update)
+	if err !=nil{
+		log.Fatal(err)
+	}
+
+	fmt.Println("modified count:",result.ModifiedCount)
+}
+
