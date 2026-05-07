810adbc088d4d6253ad89ce1146a785c982dafd6
Mahipal Panwar <mahipalpanwar6354@gmail.com>
2026-05-07 13:52:00 +0530

    DB connection

--- controller/controller.go
+++ controller/controller.go
@@ -1,6 +1,39 @@
 package controller
 
-const connectionString = "mongodb+srv://mahipal:mongodb@123123@cluster0.qskv2lv.mongodb.net/?appName=Cluster0"
-const dbname = "netflix"
-const colname = "watchlist"
+import (
+	"fmt"
+	"log"
 
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+const connectionString = "mongodb+srv://mahipal:mongodb%40123123@cluster0.qskv2lv.mongodb.net/?appName=Cluster0"
+const dbName = "netflix"
+const colName = "watchlist"
+
+// db reference
+
+var collection *mongo.Collection
+
+// connecting with MongoDB
+
+// init runs first and only one time
+func init() {
+	// client option
+
+	clientOption := options.Client().ApplyURI(connectionString)
+
+	client, err := mongo.Connect(clientOption)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Mongodb conncetion is successful ")
+
+	collection = client.Database(dbName).Collection(colName)
+
+	// collection instance
+
+	fmt.Println("collection instance is ready")
+}
