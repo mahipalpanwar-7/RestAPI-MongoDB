864740b36fc7283477e136d0d646c714e4e2f9c3
Mahipal Panwar <mahipalpanwar6354@gmail.com>
2026-05-08 14:00:45 +0530

    tiny changes (password)

--- controller/controller.go
+++ controller/controller.go
@@ -15,7 +15,7 @@ import (
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
-const connectionString = "mongodb+srv://mahipal:mongodb%40123123@cluster0.qskv2lv.mongodb.net/?appName=Cluster0"
+const connectionString = "mongodb+srv://mahipal:mahipal123123@cluster0.mrlzb9x.mongodb.net/?appName=Cluster0"
 const dbName = "netflix"
 const colName = "watchlist"
 
